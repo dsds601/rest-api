c0485bebca6b1ed9ed47fc199950a59b4fb352fd
gunho Kang <dsds601@naver.com>
2022-01-22 04:40:45 +0900

    message

--- src/main/java/me/whiteship/demoinfleanrestapi/DemoInfleanRestApiApplication.java
+++ src/main/java/me/whiteship/demoinfleanrestapi/DemoInfleanRestApiApplication.java
@@ -8,6 +8,8 @@ public class DemoInfleanRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoInfleanRestApiApplication.class, args);
+		var a = 0;
+
 	}
 
 }
