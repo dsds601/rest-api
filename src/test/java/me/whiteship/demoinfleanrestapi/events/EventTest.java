b38beae65a1a99cd37a8e616fd2c4567654e6759
gunho Kang <dsds601@naver.com>
2022-01-22 05:07:39 +0900

    event domain

---
+++ src/test/java/me/whiteship/demoinfleanrestapi/events/EventTest.java
@@ -0,0 +1,30 @@
+package me.whiteship.demoinfleanrestapi.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                        .name("Inflean Spring REST API")
+                        .description("REST API developement with spring")
+                        .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        Event event = new Event();
+        String name = "Event";
+        String description = "spring";
+
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
