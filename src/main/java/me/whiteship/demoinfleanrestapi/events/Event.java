c6f8f2cfdaf685a8a5638b6f5aec6673c9a8d615
gunho Kang <dsds601@naver.com>
2022-01-22 18:48:25 +0900

    api-events return

--- src/main/java/me/whiteship/demoinfleanrestapi/events/Event.java
+++ src/main/java/me/whiteship/demoinfleanrestapi/events/Event.java
@@ -2,6 +2,7 @@ package me.whiteship.demoinfleanrestapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.security.cert.CertPathBuilder;
 import java.time.LocalDateTime;
 
@@ -11,8 +12,10 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -26,6 +29,7 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 }
---
+++ src/test/java/me/whiteship/demoinfleanrestapi/events/EventControllerTest.java
@@ -0,0 +1,59 @@
+package me.whiteship.demoinfleanrestapi.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("RESRT API Development with spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021,02,11,17,10))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021,02,21,17,10))
+                .endEventDateTime(LocalDateTime.of(2021,02,24,18,10))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트 팩토리")
+                .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+    }
+
+}
