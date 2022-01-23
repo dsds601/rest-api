75cf89cfed6a9dbff6756ef6ad190dfd837d9dd5
gunho Kang <dsds601@naver.com>
2022-01-23 18:41:22 +0900

    api-events businessLogic

--- src/test/java/me/whiteship/demoinfleanrestapi/events/EventTest.java
+++ src/test/java/me/whiteship/demoinfleanrestapi/events/EventTest.java
@@ -27,4 +27,44 @@ class EventTest {
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(description);
     }
+
+    @Test
+    public void testFree(){
+        Event event = Event.builder()
+                .basePrice(0)
+                .maxPrice(0)
+                .build();
+
+        event.update();
+
+        assertThat(event.isFree()).isTrue();
+
+        event = Event.builder()
+                .basePrice(100)
+                .maxPrice(0)
+                .build();
+
+        event.update();
+
+        assertThat(event.isFree()).isFalse();
+    }
+
+    @Test
+    public void testOffline(){
+
+        Event event = Event.builder()
+                .location("강남역 네이버 D2 startUp Factory")
+                .build();
+
+        event.update();
+
+        assertThat(event.isOffline()).isTrue();
+
+        event = Event.builder()
+                .build();
+
+        event.update();
+
+        assertThat(event.isOffline()).isFalse();
+    }
 }
